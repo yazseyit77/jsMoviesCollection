6661f0fd532b00506ee598c2c119282bb8e88af3
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-12-14 15:21:39 -0800

    on movie title click, displayed that movie in new page

--- js-frontend/src/components/movie.js
+++ js-frontend/src/components/movie.js
@@ -1,6 +1,8 @@
 class Movie {
     constructor() {
         this.eachMovie();
+        // this.getMovieData();
+        // this.oneMovieCard();
     }
 
     eachMovie() {
@@ -8,14 +10,60 @@ class Movie {
         eachCard.addEventListener('click', e => {
             if (e.target.className === "movie-title") {
                 // console.log(e)
-                const movieId = e.target.parentElement.parentElement.parentElement.parentElement.parentElement
-                debugger
+                const columnCard = document.querySelector('.row');
+                columnCard.style.display = 'none'
+
+                const movieId = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id
+                this.getMovieData(movieId);
             }
         })
 
     } //eachMovie func
 
+    getMovieData(movieId) {
+        const url = `http://localhost:3000/api/v1/movies/${movieId}`
+        fetch(url).then(resp => resp.json()).then(movie => {
+            // this.oneMovieCard(movie)
+            const columnCardDisplay = document.querySelector('.container');
+            columnCardDisplay.innerHTML += this.oneMovieCard(movie);
+        });
+
+    } //getMovieData func
+
+
+    oneMovieCard(movie) {
+        return `
+        <div class="col-md-12">
+            <div class="card" id="">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="${movie.url}" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="${movie.url}" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">${movie.name} (${movie.released})</p>
+                            <p class="subtitle is-6">${movie.genre}</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <p>${movie.plot}</p><n /> 
+                        <p>IMDB Rating: ${movie.ratings}</p>
+                    </div>
+                    </div>
+            </div>
+        </div>
+        `;
+    } //onMovieCard func
+
 
 } //class closing brackets
+
 
 const clickTitle = new Movie();
